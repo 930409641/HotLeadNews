5ecd0fd1d6b53e72ed63895cb61e8ce160c832ce
yxq <930409641@qq.com>
2023-09-06 19:46:17 +0800

    搭建项目框架

---
+++ leadnews-common/src/main/java/com/yxq/common/exception/CustomException.java
@@ -0,0 +1,16 @@
+package com.yxq.common.exception;
+
+import com.yxq.model.common.enums.AppHttpCodeEnum;
+
+public class CustomException extends RuntimeException {
+
+    private AppHttpCodeEnum appHttpCodeEnum;
+
+    public CustomException(AppHttpCodeEnum appHttpCodeEnum){
+        this.appHttpCodeEnum = appHttpCodeEnum;
+    }
+
+    public AppHttpCodeEnum getAppHttpCodeEnum() {
+        return appHttpCodeEnum;
+    }
+}
---
+++ leadnews-service/leadnews-user/src/main/java/com/yxq/user/UserApplication.java
@@ -0,0 +1,20 @@
+package com.yxq.user;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Description:
+ * @Author: yxq
+ * @Date: 2023/9/6
+ */
+@MapperScan("com.yxq.user.mapper")
+@EnableDiscoveryClient
+@SpringBootApplication
+public class UserApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class,args);
+    }
+}
